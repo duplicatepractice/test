6ef6035f62dc3673322104e46491cfaa5458dab5
duplicatepractice <umamaheshwari.attada@gmail.com>
2025-01-12 13:05:57 +0530

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry=input("Are you hungry?")
 if hungry=="yes" or hungry=="y" or hungry=Y:
     print("Eat something")
     print("Eat idly")
+    print("sambar")
 else:
     print("Do your homework")
